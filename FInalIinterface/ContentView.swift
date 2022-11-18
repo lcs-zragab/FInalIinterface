3329aff0afc54f7f092b0c4811aea43e944b0890
zaid ragab <zragab@lcs.on.ca>
2022-11-17 20:52:07 -0500

    10/10 great comment

--- FInalIinterface/ContentView.swift
+++ FInalIinterface/ContentView.swift
@@ -9,11 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                Circle()
+                    .frame(width: 360, height: 360)
+        
+                Circle()
+                    .frame(width: 345, height: 345, alignment: .center)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("01:54")
+                        .font(.largeTitle)
+                    Image(systemName: "bell")
+                    Text("12:25")
+                }
+            }
+            
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 120, height: 120)
+                    Circle()
+                        .frame(width: 110, height: 110)
+                        .foregroundColor(.white)
+                    Text("Cancel")
+                        .font(.title2)
+                }
+                .padding()
+                Spacer()
+                ZStack {
+                    Circle()
+                        .frame(width: 120, height: 120)
+                    Circle()
+                        .frame(width: 110, height: 110)
+                        .foregroundColor(.white)
+                    Text("Pause")
+                        .font(.title2)
+                }
+                .padding()
+            }
+            Spacer()
+            HStack {
+                VStack {
+                    Image(systemName: "globe")
+                    Text("World Clock")
+                }
+                VStack {
+                    Image(systemName: "alarm")
+                    Text("Alarm")
+                }
+                VStack {
+                    Image(systemName: "stopwatch")
+                    Text("Stopwatch")
+                }
+                VStack {
+                    Image(systemName: "timer")
+                    Text("Timer")
+                }
+            }
         }
         .padding()
     }
