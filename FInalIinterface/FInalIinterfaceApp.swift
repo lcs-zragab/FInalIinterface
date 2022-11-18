889289aee382e23154c29426c6af8b7a79bdc4e9
zaid ragab <zragab@lcs.on.ca>
2022-11-17 20:14:25 -0500

    Initial Commit

---
+++ FInalIinterface/FInalIinterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FInalIinterfaceApp.swift
+//  FInalIinterface
+//
+//  Created by Zaid Ragab on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct FInalIinterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
