9450dc328fad6d4ab469df87dd74626b501902cf
zaid ragab <zragab@lcs.on.ca>
2022-11-18 09:14:23 -0500

    I got the background to bed black

--- FInalIinterface/ContentView.swift
+++ FInalIinterface/ContentView.swift
@@ -9,69 +9,75 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      
-        VStack {
-            ZStack {
-                Circle()
-                    .frame(width: 360, height: 360)
         
-                Circle()
-                    .frame(width: 345, height: 345, alignment: .center)
-                    .foregroundColor(.white)
-                VStack {
-                    Text("01:54")
-                        .font(.largeTitle)
-                    Image(systemName: "bell")
-                    Text("12:25")
-                }
-            }
-            
-            HStack {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
                 ZStack {
                     Circle()
-                        .frame(width: 120, height: 120)
+                        .frame(width: 360, height: 360)
+                    
                     Circle()
-                        .frame(width: 110, height: 110)
+                        .frame(width: 345, height: 345, alignment: .center)
                         .foregroundColor(.white)
-                    Text("Cancel")
-                        .font(.title2)
+                    VStack {
+                        Text("01:54")
+                            .font(.largeTitle)
+                        Image(systemName: "bell")
+                        Text("12:25")
+                    }
                 }
-                .padding()
+                
+                HStack {
+                    ZStack {
+                        Circle()
+                            .frame(width: 120, height: 120)
+                        Circle()
+                            .frame(width: 110, height: 110)
+                            .foregroundColor(.white)
+                        Text("Cancel")
+                            .font(.title2)
+                    }
+                    .padding()
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .frame(width: 120, height: 120)
+                        Circle()
+                            .frame(width: 110, height: 110)
+                            .foregroundColor(.white)
+                        Text("Pause")
+                            .font(.title2)
+                    }
+                    .padding()
+                }
                 Spacer()
-                ZStack {
-                    Circle()
-                        .frame(width: 120, height: 120)
-                    Circle()
-                        .frame(width: 110, height: 110)
-                        .foregroundColor(.white)
-                    Text("Pause")
-                        .font(.title2)
-                }
-                .padding()
-            }
-            Spacer()
-            HStack {
-                VStack {
-                    Image(systemName: "globe")
-                    Text("World Clock")
-                }
-                VStack {
-                    Image(systemName: "alarm")
-                    Text("Alarm")
-                }
-                VStack {
-                    Image(systemName: "stopwatch")
-                    Text("Stopwatch")
-                }
-                VStack {
-                    Image(systemName: "timer")
-                    Text("Timer")
+                HStack {
+                    VStack {
+                        Image(systemName: "globe")
+                        Text("World Clock")
+                    }
+                    VStack {
+                        Image(systemName: "alarm")
+                        Text("Alarm")
+                    }
+                    VStack {
+                        Image(systemName: "stopwatch")
+                        Text("Stopwatch")
+                    }
+                    VStack {
+                        Image(systemName: "timer")
+                        Text("Timer")
+                    }
                 }
             }
+            .padding()
         }
-        .padding()
+        
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
